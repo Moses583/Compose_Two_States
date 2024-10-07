f5b24af20d60e8bdef2476480a07ee8aca62415a
Moses583 <moisendungu@gmail.com>
2024-10-07 12:10:52 +0300

    Second commit

--- app/src/main/java/com/ravemaster/composetwostates/MainActivity.kt
+++ app/src/main/java/com/ravemaster/composetwostates/MainActivity.kt
@@ -6,22 +6,23 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.ravemaster.composetwostates.ui.theme.ComposeTwoStatesTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,37 +32,72 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeTwoStatesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    var textState by remember {
+                        mutableStateOf("")
+                    }
+
+                    MyTextField2(textValue =textState,
+                        onValueChanged = { textState = it },
+                        modifier = Modifier.padding(innerPadding),
+                        onAddClick = {
+
+                        })
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting( modifier: Modifier = Modifier) {
-    Box(modifier = modifier
-        .fillMaxSize()
-        .background(MaterialTheme.colorScheme.surface),
-        ){
-        TextField(value = "",
-            onValueChange = {
+ @Composable
+ fun MyTextField(modifier: Modifier = Modifier) {
+     Box(modifier = modifier
+         .fillMaxSize()
+         .background(MaterialTheme.colorScheme.surface),
+     ){
+         //State controlled by composable:Stateful
 
-            },
-            modifier = modifier
-                .fillMaxWidth()
-                .align(Alignment.BottomCenter)
+         var textState by remember {
+             mutableStateOf("")
+         }
 
-        )
-    }
-}
+         TextField(value = textState,
+             onValueChange = {
+                 textState = it
+             },
+             modifier = modifier
+                 .fillMaxWidth()
+                 .align(Alignment.BottomCenter)
+
+         )
+     }
+ }
+ @Composable
+ fun MyTextField2(modifier: Modifier = Modifier, textValue: String, onValueChanged: (String) -> Unit, onAddClick: () -> Unit) {
+     //State controlled from outside: Stateless
+     Box(modifier = modifier
+         .fillMaxSize()
+         .background(MaterialTheme.colorScheme.surface),
+     ){
+
+         TextField(value = textValue,
+             onValueChange = {
+                 onValueChanged(it)
+             },
+             modifier = modifier
+                 .fillMaxWidth()
+                 .align(Alignment.BottomCenter),
+             trailingIcon = {
+                 Icon(imageVector = Icons.Default.Add, contentDescription = null)
+             }
+
+         )
+     }
+ }
 
 @Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ComposeTwoStatesTheme {
-        Greeting()
+//        Greeting(textState)
     }
 }
