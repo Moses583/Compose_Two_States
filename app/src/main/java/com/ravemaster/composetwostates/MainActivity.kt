87b94ea5a0284d6d7efb0f7595c79ec435825464
Moses583 <moisendungu@gmail.com>
2024-10-07 10:35:57 +0300

    Initial commit

---
+++ app/src/main/java/com/ravemaster/composetwostates/MainActivity.kt
@@ -0,0 +1,67 @@
+ package com.ravemaster.composetwostates
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.ravemaster.composetwostates.ui.theme.ComposeTwoStatesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeTwoStatesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting( modifier: Modifier = Modifier) {
+    Box(modifier = modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.surface),
+        ){
+        TextField(value = "",
+            onValueChange = {
+
+            },
+            modifier = modifier
+                .fillMaxWidth()
+                .align(Alignment.BottomCenter)
+
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTwoStatesTheme {
+        Greeting()
+    }
+}
